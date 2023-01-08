17e065a5de6c546b960992ad4fea389105b7c5ac
Paxwell <pompom231245@gmail.com>
2023-01-08 22:00:32 +0700

    update not perfectcode

--- my-web-api/main.py
+++ my-web-api/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
 import mysql.connector as msql
+import time
 
 app = FastAPI() 
 
@@ -7,6 +9,9 @@ app = FastAPI()
     Run api server
     $uvicorn main:app --reload 
 """
+
+app.add_middleware(CORSMiddleware, allow_origins=["http://127.0.0.1:5500"], allow_methods=["GET","POST","PUT","DELETE"])
+
 
 @app.get("/hello-my-web")
 def hello_my_web():
@@ -29,31 +34,79 @@ def test_list():
 
 @app.get("/get-all-income")
 def getAllIncome():
+    start = time.time()
     mydb = msql.connect(host="localhost", user="root", password="root", port="8889", database="my-web")
     cursor = mydb.cursor(dictionary=True)
-    cursor.execute("select * from income_outcome")
-    result = cursor.fetchall()
-    return result
+    error_msg =""
+    result = []
+    try:
+        cursor.execute("select * from income_outcome")
+        result = cursor.fetchall()
+    except Exception as e:
+        error_msg =str(e)
+    cursor.close()
+    mydb.close()
+    took = time.time()-start
+    return {"data": result, "took":took ,"error_msg":error_msg}
 
 @app.post("/insert-income")  
 def insert_sql(desc: str = Form(),amount: float =Form(),date:  str = Form() ):
+    start = time.time()
     mydb = msql.connect(host="localhost", user="root", password="root", port="8889", database="my-web")
     cursor = mydb.cursor()
-    cursor.execute(f"insert into income_outcome(description,amount,date) values ('{desc}',{amount},'{date}')")
-    mydb.commit()
-    
-    rowcount = cursor.rowcount
-    return {"rowcount": rowcount}
+    rowcount = 0
+    error_msg= ""
+    try:
+        cursor.execute(f"insert into income_outcome(description,amount,date) values ('{desc}',{amount},'{date}')")
+        mydb.commit()
+        rowcount = cursor.rowcount
+    except Exception as e:
+        error_msg =str(e)
+    cursor.close()
+    mydb.close()
+    took = time.time()-start
+    return {"rowcount": rowcount,"took": took,"error_msg":error_msg}
+
 @app.delete("/delete-income")  
 def delete_sql(id: int = Form()):
+    start = time.time()
     mydb = msql.connect(host="localhost", user="root", password="root", port="8889", database="my-web")
     cursor = mydb.cursor()
-    cursor.execute(f"delete from income_outcome where id={id}")
-    mydb.commit()
+    error_msg=""
+    rowcount =0
+    try:
+        cursor.execute(f"delete from income_outcome where id={id}")
+        mydb.commit()
+        rowcount = cursor.rowcount
+    except Exception as e:
+        error_msg = str(e)
+    cursor.close()
+    mydb.close()
+    took = time.time()-start
+    return {"rowcount": rowcount,"took":took,"error_msg":error_msg}  
     
-    rowcount = cursor.rowcount
-    return {"rowcount": rowcount}  
+@app.put("/update-income")
+def update_sql(id: int =Form(),desc: str = Form(),amount: float =Form(),date:  str = Form()):
+    start_time = time.time()
+    err_msg = ""
+    rowcount = 0
+    mydb = msql.connect(host="localhost", user="root", password="root", port="8889", database="my-web")
+    cursor = mydb.cursor()
     
+    try:
+        sql = f"""update income_outcome set description = '{desc}',amount = {amount},date = '{date}'  
+                    where id = {id}
+                """
+        cursor.execute(sql)
+        mydb.commit()
+        rowcount = cursor.rowcount
+    except Exception as e:
+        err_msg = str(e)
+
+    cursor.close()
+    mydb.close()
+    took = time.time() - start_time
+    return {"rowcount": rowcount, "took": took, "err_msg": err_msg}
 
 
 
